2925989b7314396dc2908c8d0ad04c25d9097474
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-04-15 23:04:46 -0300

    server of application

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import express from 'express';
+import "reflect-metadata";
+import express from "express";
 
 const server = express();
-/**
- * Instrui o uso do JSON pelo Node
- */
+
 server.use(express.json());
 
-server.listen(process.env.PORT || 3000, () => console.log("Server is Running!"))
+server.listen(process.env.PORT || 3000, () => console.log("Server is Running!"));

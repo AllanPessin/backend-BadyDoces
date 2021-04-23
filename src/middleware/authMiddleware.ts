468a34df1343a9feeffb3ffa2ed563d76a4963ce
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-04-23 16:26:21 -0300

    athuenticate middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const authmiddleware = async (request: Request, response: Response, next: NextFunction) => {
+  const authorization = request.headers.authorization;
+
+  if (!authorization) {
+    return response.status(400).json({
+      error: "Token is required!"
+    });
+  };
+
+  const token = authorization.replace("Bearer", "").trim();
+
+  try {
+    await jwt.verify(token, process.env.SECRET);
+    next();
+  } catch (erro) {
+    return response.status(400).json({
+      error: "Token invalid!"
+    });
+  };
+};

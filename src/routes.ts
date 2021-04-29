749541b30fea35a111b170011d486c70da804fb0
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-04-29 16:33:14 -0300

    new routes to product

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { AdminController } from "./controllers/AdminController";
 import { CategoryController } from "./controllers/CategoryController";
+import { ProductController } from "./controllers/ProductController";
 import { SaleController } from "./controllers/SaleController";
 import { authmiddleware } from "./middleware/authMiddleware";
 
@@ -9,6 +10,7 @@ const router = Router();
 const adminController = new AdminController();
 const saleController = new SaleController();
 const categoryController = new CategoryController();
+const productController = new ProductController();
 
 router.get("/", (request: Request, response: Response) => {
   return response.json({ message: "Bady Doces API" });
@@ -35,6 +37,12 @@ router.post("/new-category", categoryController.create);
 router.get("/show-category", categoryController.show);
 router.patch("/update-category/:name", categoryController.update);
 router.delete("/delete-category/:id", categoryController.delete);
+
+router.post("/new-product", productController.create);
+router.get("/show-product", productController.show);
+router.patch("/update-product/:id", productController.update);
+router.get("/show-product/:id", productController.index);
+router.delete("/delete-product/:id", productController.delete);
 
 export { router };
 

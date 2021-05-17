d6c3987303b10e951299f89b6a22365729c3b01b
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-05-17 11:18:41 -0300

    SSl to database

--- ormconfig.js
+++ ormconfig.js
@@ -5,10 +5,10 @@ module.exports = {
     rejectUnauthorized: false,
   },
   entities: [
-    "src/models/**{.ts,.js}",
+    "dist/models/**{.ts,.js}",
   ],
   migrations: [
-    "src/database/migrations/**{.ts,.js}",
+    "dist/database/migrations/**{.ts,.js}",
   ],
   cli: {
     migrationsDir: "src/database/migrations",

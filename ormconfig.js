aa38396d81918b4af2ebb10c7b28a8e3128b6c2e
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-06-06 12:09:46 -0300

    orm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,14 @@
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: [
-    "src/models/**.ts",
+    "dist/models/**.js",
   ],
   migrations: [
-    "src/database/migrations/**.ts",
+    "dist/database/migrations/**.js",
   ],
   cli: {
     migrationsDir: "src/database/migrations",
